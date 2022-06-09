7028971a586216a38d60cee8bf48792e389d2bad
jingood2 <jingood2@gmail.com>
2022-06-09 23:29:56 +0900

    add eks product

---
+++ projects/eks/src/main.ts
@@ -0,0 +1,68 @@
+import { App, Stack, StackProps } from 'aws-cdk-lib';
+import * as servicecatalog from 'aws-cdk-lib/aws-servicecatalog';
+import * as iam from 'aws-cdk-lib/aws-iam';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import { Construct } from 'constructs';
+import { envVars } from './config';
+
+export class MyStack extends Stack {
+  readonly portfolio: servicecatalog.IPortfolio;
+  constructor(scope: Construct, id: string, props: StackProps = {}) {
+    super(scope, id, props);
+
+    if (envVars.SC_PORTFOLIO_ARN != '') {
+      this.portfolio = servicecatalog.Portfolio.fromPortfolioArn(this, 'ImportedNetworkPortfolio', envVars.SC_PORTFOLIO_ARN);
+    } else {
+      this.portfolio = new servicecatalog.Portfolio(this, envVars.SC_PORTFOLIO_NAME, {
+        displayName: envVars.SC_PORTFOLIO_NAME ?? 'DemoPortfolio',
+        providerName: 'SK Cloud Transformation Group',
+        description: 'AWS Service Catalog EKS Product ',
+        messageLanguage: servicecatalog.MessageLanguage.EN,
+      });
+
+      if (envVars.SC_ACCESS_GROUP_NAME != '') {
+        const group = iam.Group.fromGroupName(this, 'SCGroup', envVars.SC_ACCESS_GROUP_NAME);
+        this.portfolio.giveAccessToGroup(group);
+      }
+
+      if (envVars.SC_ACCESS_ROLE_ARN != '') {
+        this.portfolio.giveAccessToRole(iam.Role.fromRoleArn(this, `${envVars.SC_PORTFOLIO_NAME}-Role`, envVars.SC_ACCESS_ROLE_ARN));
+      } else {
+        this.portfolio.giveAccessToRole(iam.Role.fromRoleArn(this, `${envVars.SC_PORTFOLIO_NAME}AdminRole`, `arn:aws:iam::${process.env.CDK_DEFAULT_ACCOUNT}:role/AssumableAdminRole`));
+      }
+
+      // Create a product from a stack
+      class S3BucketProduct extends servicecatalog.ProductStack {
+        constructor(scope: Construct, id: string) {
+          super(scope, id);
+
+          new s3.Bucket(this, 'BucketProduct');
+        }
+      }
+
+      const product = new servicecatalog.CloudFormationProduct(this, 'Product', {
+        productName: "My Product",
+        owner: "Product Owner",
+        productVersions: [
+          {
+            productVersionName: "v1",
+            cloudFormationTemplate: servicecatalog.CloudFormationTemplate.fromProductStack(new S3BucketProduct(this, 'S3BucketProduct')),
+          },
+        ],
+      });
+    }
+  }
+}
+
+// for development, use account/region from cdk cli
+const devEnv = {
+  account: process.env.CDK_DEFAULT_ACCOUNT,
+  region: process.env.CDK_DEFAULT_REGION,
+};
+
+const app = new App();
+
+new MyStack(app, 'eks-dev', { env: devEnv });
+// new MyStack(app, 'eks-prod', { env: prodEnv });
+
+app.synth();
--- projects/github-oidc-provider/src/main.ts
+++ projects/github-oidc-provider/src/main.ts
@@ -1,7 +1,7 @@
-import { GithubActionsIdentityProvider, GithubActionsRole } from "aws-cdk-github-oidc";
-import { App, Duration, Stack, StackProps } from "aws-cdk-lib";
-import * as iam from "aws-cdk-lib/aws-iam";
-import { Construct } from "constructs";
+import { GithubActionsIdentityProvider, GithubActionsRole } from 'aws-cdk-github-oidc';
+import { App, Duration, Stack, StackProps } from 'aws-cdk-lib';
+import * as iam from 'aws-cdk-lib/aws-iam';
+import { Construct } from 'constructs';
 
 export class MyStack extends Stack {
   constructor(scope: Construct, id: string, props: StackProps = {}) {
@@ -11,18 +11,29 @@ export class MyStack extends Stack {
      * Create an Identity provider for GitHub inside your AWS Account. This
      * allows GitHub to present itself to AWS IAM and assume a role.
      */
-    const provider = new GithubActionsIdentityProvider(this, "GithubProvider");
+    const provider = new GithubActionsIdentityProvider(this, 'GithubProvider');
 
-    const deployRole = new GithubActionsRole(this, "GithubDeployRole", {
+    const deployRole = new GithubActionsRole(this, 'GithubDeployRole', {
       provider: provider, // reference into the OIDC provider
-      owner: "jingood2", // your repository owner (organization or user) name
-      repo: "github-monorepo-ci", // your repository name (without the owner name)
-      roleName: "github-deploy-v2-role",
-      description: "This role deploys stuff to AWS v2",
+      owner: 'jingood2', // your repository owner (organization or user) name
+      repo: 'monorepo-cdk-project', // your repository name (without the owner name)
+      roleName: 'github-monorepo-cdk-role',
+      description: 'This role deploys stuff to AWS v2',
       maxSessionDuration: Duration.hours(2),
     });
 
-    deployRole.addManagedPolicy(iam.ManagedPolicy.fromAwsManagedPolicyName("AdministratorAccess"));
+    deployRole.addManagedPolicy(iam.ManagedPolicy.fromAwsManagedPolicyName('AdministratorAccess'));
+
+    const deploy2Role = new GithubActionsRole(this, 'GithubDeploy2Role', {
+      provider: provider, // reference into the OIDC provider
+      owner: 'jingood2', // your repository owner (organization or user) name
+      repo: 'monorepo-actions-ci', // your repository name (without the owner name)
+      roleName: 'github-monorepo-actions-ci-role',
+      description: 'This role deploys stuff to AWS v2',
+      maxSessionDuration: Duration.hours(2),
+    });
+
+    deploy2Role.addManagedPolicy(iam.ManagedPolicy.fromAwsManagedPolicyName('AdministratorAccess'));
   }
 }
 
@@ -34,6 +45,6 @@ const devEnv = {
 
 const app = new App();
 
-new MyStack(app, "bootstrap-dev", { env: devEnv });
+new MyStack(app, 'bootstrap-dev', { env: devEnv });
 
 app.synth();
